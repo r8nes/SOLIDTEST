2aa44016264f88dbbbb80afef353fa26648be721
Dmitry <59758691+r8nes@users.noreply.github.com>
2021-09-10 19:20:35 +0300

    SRP Code

---
+++ 1 - SRP/SRP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.SRP
+{
+    public class Vampire
+    {
+        private string _name;
+        private int _age;
+
+        public Vampire(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetAge() {
+            return _age;
+        }
+    }
+
+    public static class VampireInfo
+    {
+        public static void Check(Vampire vampire)
+        {
+            Console.WriteLine($"Vampire name is {vampire.GetName()}. It's {vampire.GetAge()} age old\n");
+        }
+
+        public static void Check(List<Vampire> vampires) {
+            foreach (Vampire vampire in vampires)
+            {
+                Console.WriteLine($"Vampire name is {vampire.GetName()}. It's {vampire.GetAge()} age old\n");
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using SOLID.SRP;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Single Responsibility Principle
+
+
+            Vampire edwardCullen = new Vampire("Edward", 112);
+            Vampire bellaCullen = new Vampire("Bella", 18);
+
+            VampireInfo.Check(edwardCullen);
+            VampireInfo.Check(bellaCullen);
+
+//=============================================================
+            
+            List<Vampire> cullenClan = new List<Vampire>();
+            
+            cullenClan.Add(edwardCullen);
+            cullenClan.Add(bellaCullen);
+
+            VampireInfo.Check(cullenClan);
+        }
+    }
+}
